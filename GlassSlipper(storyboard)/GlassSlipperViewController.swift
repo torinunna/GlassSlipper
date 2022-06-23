02a667d5bb640e00eb011bfbd30f63cfa2afc1ec
torinunna <torinunna9588@gmail.com>
2022-06-24 00:56:47 +0900

    Add TabelViewController Set GlassSlipperViewController

--- GlassSlipper(storyboard)/GlassSlipperViewController.swift
+++ GlassSlipper(storyboard)/GlassSlipperViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GlassSlipperViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
