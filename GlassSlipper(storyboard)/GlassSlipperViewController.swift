9185f230997f9ece9f9c16c5f8e7794f2d5efb6e
torinunna <torinunna9588@gmail.com>
2022-06-24 15:24:55 +0900

    Add itemArray for hardcoding Set Tableview Datasource Methods

--- GlassSlipper(storyboard)/GlassSlipperViewController.swift
+++ GlassSlipper(storyboard)/GlassSlipperViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class GlassSlipperViewController: UITableViewController {
 
+    
+    let itemArray = ["지갑", "애플워치", "에어팟"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    
+//    Mark - Tableview Datasource Methods
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GlassSlipperCell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = itemArray[indexPath.row]
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
 
 }
 
