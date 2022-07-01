818dd6022699ea3339f0d9fafb39518305ae65bb
torinunna <torinunna9588@gmail.com>
2022-07-01 22:04:55 +0900

    fix Search Bar error using predicates

--- GlassSlipper(storyboard)/Controllers/GlassSlipperViewController.swift
+++ GlassSlipper(storyboard)/Controllers/GlassSlipperViewController.swift
@@ -126,7 +126,15 @@ class GlassSlipperViewController: UITableViewController {
    
     
 //    read in CRUD
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
         
         do {
             itemArray = try context.fetch(request)
@@ -147,11 +155,11 @@ extension GlassSlipperViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
         
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
     
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
         
     }
     
